eec193b4d6fec613b4ba1fd22a72f7dc7641325b
aditechdev <aditechdevofficial@gmail.com>
2022-06-27 14:24:53 +0530

    :zap: Tab bar

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -7,20 +7,51 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TabController tabController;
     logOut() {
       AuthService authService = AuthService();
       authService.signOut();
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () => logOut(),
-          )
-        ],
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: const Text("Medi Buddy"),
+          bottom: TabBar(
+            padding: EdgeInsets.zero,
+            indicatorColor: Theme.of(context).errorColor,
+            tabs: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Tab(
+                  icon: Icon(Icons.cloud_outlined),
+                ),
+              ),
+              Tab(
+                icon: Icon(Icons.beach_access_sharp),
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout),
+              onPressed: () => logOut(),
+            )
+          ],
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text("It's cloudy here"),
+            ),
+            Center(
+              child: Text("It's rainy here"),
+            ),
+          ],
+        ),
       ),
     );
   }
