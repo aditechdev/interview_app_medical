736ee6f3cc9086f61df86e1c6d1a1230f6b36fe2
aditechdev <aditechdevofficial@gmail.com>
2022-06-28 17:38:52 +0530

    :sparkles: UI FIx

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -1,30 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:interview_app_medical/features/auth/services/auth_service.dart';
+import 'package:intl/intl.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName = "/home";
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  DateFormat dateFormat = DateFormat.jm();
+  String myTime = "";
+  @override
+  void didChangeDependencies() {
+    myTime = dateFormat.format(DateTime.now());
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    TabController tabController;
+    TabController _tabController = TabController(length: 2, vsync: this);
+    // TickerProvider tickerProvider = const TickerProvider();
+
     logOut() {
       AuthService authService = AuthService();
       authService.signOut();
     }
 
-    return DefaultTabController(
-      initialIndex: 1,
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          title: const Text("Medi Buddy"),
-          bottom: TabBar(
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text("Medi Buddy"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => logOut(),
+          )
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Card(
+              child: Container(
+            padding: const EdgeInsets.all(10),
+            width: double.infinity,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Hello Name"),
+                  Text(myTime),
+                ],
+              ),
+              const Text("Email ID"),
+            ]),
+          )),
+          TabBar(
+            controller: _tabController,
             padding: EdgeInsets.zero,
-            indicatorColor: Theme.of(context).errorColor,
-            tabs: <Widget>[
-              Container(
+            indicatorColor: Theme.of(context).primaryColor,
+            labelColor: Theme.of(context).primaryColor,
+            unselectedLabelColor: Colors.black87,
+            tabs: const <Widget>[
+              SizedBox(
                 width: double.infinity,
                 child: Tab(
                   icon: Icon(Icons.cloud_outlined),
@@ -35,24 +77,23 @@ class HomeScreen extends StatelessWidget {
               ),
             ],
           ),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.logout),
-              onPressed: () => logOut(),
-            )
-          ],
-        ),
-        body: const TabBarView(
-          children: <Widget>[
-            Center(
-              child: Text("It's cloudy here"),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: const <Widget>[
+                Center(
+                  child: Text("It's cloudy here"),
+                ),
+                Center(
+                  child: Text("It's rainy here"),
+                ),
+              ],
             ),
-            Center(
-              child: Text("It's rainy here"),
-            ),
-          ],
-        ),
+          )
+        ],
       ),
+
+      // const
     );
   }
 }
